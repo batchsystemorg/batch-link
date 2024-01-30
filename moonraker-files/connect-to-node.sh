60da08a9410e752895c0823d94ba0fbbbb2474e0
itymarcel <marcel@hypersuper.uk>
2024-01-30 09:56:04 +0000

    update

--- moonraker-files/connect-to-node.sh
+++ moonraker-files/connect-to-node.sh
@@ -9,4 +9,4 @@ NODE_SERVER_PORT="3777"
 
 # Your connection logic here
 # For example, you might use curl to make an HTTP request
-curl -v "$NODE_SERVER_IP:$NODE_SERVER_PORT"
+curl -v "$NODE_SERVER_IP:$NODE_SERVER_PORT/"
--- server2.js
+++ server2.js
@@ -7,9 +7,20 @@ app.get("/", (req, res) => {
   console.log('get request received')
   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(`Connected device IP: ${clientIp}`);
+  return res.type('html').send(html)
 });
 
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+  </head>
+</html>
+`
