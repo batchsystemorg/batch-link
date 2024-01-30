706ea960b46eaadda596162cf4e3614a0306b3c2
itymarcel <marcel@hypersuper.uk>
2024-01-30 09:52:10 +0000

    update server

--- moonraker-files/connect-to-node.sh
+++ moonraker-files/connect-to-node.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Replace with your Node.js server IP and port
-NODE_SERVER_IP="192.168.110.112"
-NODE_SERVER_PORT="3000"
+NODE_SERVER_IP="https://moonraker-api.onrender.com"
+NODE_SERVER_PORT="3777"
+
+# NODE_SERVER_IP="http://localhost"
+# NODE_SERVER_PORT="10000"
 
 # Your connection logic here
 # For example, you might use curl to make an HTTP request
-curl "http://$NODE_SERVER_IP:$NODE_SERVER_PORT/connect"
+curl -v "$NODE_SERVER_IP:$NODE_SERVER_PORT"
--- server2.js
+++ server2.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 10000;
 
-app.use((req, res, next) => {
+
+app.get("/", (req, res) => {
+  console.log('get request received')
   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log(`Connected device IP: ${clientIp}`);
-  next();
 });
 
-// Your other routes and server logic here
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
